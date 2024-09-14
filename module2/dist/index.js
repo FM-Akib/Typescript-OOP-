565eb211f674fb4e92525427df0629ed48b7262e
FM-Akib <muntasirakib08@gmail.com>
2024-09-14 10:30:38 +0600

    first commit

---
+++ module2/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+{
+    const abc = "my name is Object Oriented Programming";
+    console.log(abc);
+}
---
+++ module2/src/index.ts
@@ -0,0 +1,4 @@
+{
+    const abc : string = "my name is Object Oriented Programming";
+    console.log(abc);
+}
